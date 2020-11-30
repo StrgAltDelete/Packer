3e682e774644d51f6e2f0b18a62e4db2c73a4322
Yan <70441510+Yan1921@users.noreply.github.com>
2020-11-30 21:02:43 +0100

    Add files via upload

---
+++ Decoder_V1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Decoder_Alpha
+{
+    class Program
+    {
+        static void Main(string[] args)     //Ctrl K + Ctrl D
+        {
+            //Decoder someOtherClass = new Decoder();
+            //someOtherClass.someMethod();
+        }
+    }
+
+    class Decoder
+    {
+        string filename;
+        string fileCache;
+        char seperator;
+        char multiplier;
+        char tmpCache;
+        int tmpResult;
+        FileStream fs;
+        BinaryReader br;
+
+        public Decoder()    //Konstruktor
+        {
+            filename = "Test.txt";
+            fileCache = " ";
+            seperator = '³';
+            multiplier = ' ';
+            tmpCache = ' ';
+            tmpResult = 0;
+            fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            br = new BinaryReader(fs);
+        }
+
+        public void someMethod()
+        {
+            Decoder Const = new Decoder();
+            Const.fs.Position = 0;
+            while (Const.fs.Position < Const.fs.Length)
+            {
+                Const.fileCache += (char)br.ReadByte();
+            }
+
+            Console.WriteLine(fileCache);
+
+            for (int i = 0; i < fileCache.Length; i++)
+            {
+                if (i == seperator)
+                {
+                    Const.multiplier = fileCache[i + 1];
+                    Const.tmpCache = fileCache[i + 2];
+                    Const.tmpResult = (int)multiplier * (int)tmpCache;
+                    Console.WriteLine(tmpResult);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
