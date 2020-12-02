e2fb87481849403c3f01d54da51e9e47b5939cfa
Elle <62961896+StrgAltDelete@users.noreply.github.com>
2020-12-02 22:53:36 +0100

    Update Encoder.cs
    Schau mal ob dus lesen kannst

--- Encoder.cs
+++ Encoder.cs
@@ -8,16 +8,19 @@ namespace Zipper
         static void Main(string[] args)
         {
             string s_file = "";
-            while (true)                  //Schleife zur Eingabe des Dateinamens und der Überprüfung, ob diese exestiert
+            string s_originalName = "";
+            while (true)                                                                        //Schleife zur Eingabe des Dateinamens und der Überprüfung, ob diese exestiert
             {
                 Console.WriteLine("Geben Sie den Dateinamen mit Dateiendung ein");
-                s_file = Console.ReadLine();
-                if (File.Exists(s_file))  //Überprüft ob die Datei exestiert und bricht die Schleife ab wenn ja
+                s_originalName = Console.ReadLine();
+                if (File.Exists(s_originalName))                                                //Überprüft ob die Datei exestiert und bricht die Schleife ab wenn ja
                 {
+                    if (s_originalName.Length > 8)
+                        s_file = s_originalName.Remove(8);
                     break;
                 }
             }
-            Encoder encoder = new Encoder(s_file, '{');
+            Encoder encoder = new Encoder(s_originalName, s_file, '{');
             if (encoder.Exist() == true)
             {
                 encoder.Encoding();
@@ -26,143 +29,141 @@ namespace Zipper
     }
     public class Encoder
     {
-        string s_prefix = "MOIN";                               //Dateipräfix zur Identifikation als unsere Datei (MagicNumber)
-        char[] ac_ASCII;
-        char[] ac_signs = new char[4];                          //Array zum Speichern der letzten 4 Zeichen, die gelesen wurden
-        string s_file;                                          //Dateiname
-        string s_name;                                          //Dateinname für.fun
-        string s_ending;
-        char c_sign;                                            //Trennzeichen
-        FileStream fs;
+        string s_prefix = "MOIN";                                                               //Dateipräfix zur Identifikation als unsere Datei (MagicNumber)
+        string s_file;                                                                          //Dateiname
+        string s_name;                                                                          //Dateinname für.fun
+        char c_sign;                                                                            //Trennzeichen
+        FileStream fs_Write;                                                                    //Stream zum Setzen der Datei
+        FileStream fs_Read;                                                                     //Stream zum Lesen der Datei
         BinaryReader br;
         BinaryWriter bw;
 
-        public Encoder(string s_file, char c_sign)
+        public Encoder(string s_originalName, string s_name, char c_sign)                   /*Konstruktor*/
         {
-            this.s_file = s_file;
+            this.s_file = s_originalName;
             this.c_sign = c_sign;
-            Name();
+            Name(s_name);
         }
-        private void Name()                                     /*Name der Datei speichern*/
+        private void Name(string s_name)                                                    /*Name der Datei speichern*/
         {
-            string s_name = "";
-            string s_ending = "";
-            bool b_ending = false;
-            foreach (char c in s_file)
-            {
-                if (c == '.')                                   //Wenn . ist, also wenn die Datei endet, wird die Dateiendung extra gespeichert
-                    b_ending = true;
-                if (b_ending == false)
-                    s_name += c;
-                else
-                    s_ending += c;
-            }
-            s_name += ".fun";                                   //Gefilterter Name + neue Dateiendung
+            s_name += ".fun";                                                                   //Gekürtzer Name + neue Dateiendung
             this.s_name = s_name;
-            this.s_ending = s_ending;
         }
-        public bool Exist()                                     /*Überprüft ob die .fun Datei bereits exestiert und gibt false zurück wenn sie bereits exestiert und true wenn nicht*/
+        public bool Exist()                                                                 /*Überprüft ob die .fun Datei bereits exestiert und gibt false zurück wenn sie bereits exestiert und true wenn nicht*/
         {
-            if (!File.Exists(s_name))                           //Überprüft ob die neue .fun Datei bereits exestiert
-                return true;                                    //Gibt true zurück, da mit dem encoden fortgefahren werden kann
+            if (!File.Exists(s_name))                                                           //Überprüft ob die neue .fun Datei bereits exestiert
+                return true;                                                                    //Gibt true zurück, da mit dem encoden fortgefahren werden kann
             else
-                return false;                                   //Gibt false zurück, da die Datei überschrieben werden würde
+                return false;                                                                   //Gibt false zurück, da die Datei überschrieben werden würde
         }
-        public void Encoding()                                  /*Ruft alle notwendigen Methoden zum encoden auf*/
+        public void Encoding()                                                              /*Ruft alle notwendigen Methoden zum encoden auf*/
         {
-            Read();
             Prefix();
             Algorythm();
             Done();
         }
-        private void Read()                                     /*Liest den Inhalt der zu packenden Datei und speichert ihn in einem Char-Array*/
-        {
-            fs = new FileStream(s_file, FileMode.Open);
-            br = new BinaryReader(fs);
-            ac_ASCII = new char[fs.Length];
-            for (long l = 0; fs.Position < fs.Length; l++)
-            {
-                ac_ASCII[l] += (char)br.ReadByte();
-            }
-            fs.Flush();
-            br.Close();
-            fs.Close();
-        }
-        private void Prefix()
+        private void Prefix()                                                               /*Ruft alle notwendigen Methoden zum setzen des Headers auf*/
         {
             MagicNumber();
             Seperator();
             Ending();
         }
-        private void MagicNumber()
+        private void MagicNumber()                                                          /*Identikator, dass es unsere .fun-Datei ist*/
         {
-            fs = new FileStream(s_name, FileMode.Create);
-            br = new BinaryReader(fs);
-            bw = new BinaryWriter(fs);
-            fs.Position = 0;
-            foreach (char c in s_prefix)
+            fs_Write = new FileStream(s_name, FileMode.Create);                             
+            bw = new BinaryWriter(fs_Write);
+            foreach (char c in s_prefix)                                                        //Setzt unsere MagicLine
                 bw.Write(c);
         }
-        private void Ending()
+        private void Ending()                                                               /*Schreibt den evtl gekürtzten Dateinamen in den Header*/
         {
-            foreach (char c in s_ending)
+            foreach (char c in s_name)
+            {
+                bool b_dot = false;
+                if (c == '.')
+                {
+                    b_dot = true;
+                    bool b_dot2 = false;
+                    foreach (char c2 in s_file)
+                    {
+                        if (c2 == '.')
+                            b_dot2 = true;
+                        if (b_dot2 == true)
+                            bw.Write(c2);
+                    }
+                }
+                if (b_dot == true)
+                    break;
                 bw.Write(c);
-            bw.Write('[');                                      //Identikator für das Ende der Dateiendung (Festgelegt)
+            }
+            bw.Write('[');                                                                      //Identikator für das Ende des Namens der originellen Datei (Festgelegt)
         }
-        private void Seperator()
+        private void Seperator()                                                            /*Schreibt das Trennzeichen in den Header*/
         {
             bw.Write(c_sign);
         }
-        private void Done()                                     /*Schliesst und leer alle offenen Streams*/
+        private void Done()                                                                 /*Schliesst und leer alle offenen Streams*/
         {
             bw.Flush();
             bw.Close();
             br.Close();
-            fs.Close();
+            fs_Write.Close();
+            fs_Read.Close();
         }
-        private void Algorythm()
+        private void Algorythm()                                                            /*Der endgültige Decoder-Algorythmus*/
         {
-            byte b_sameSigns = 1;                                //Zählt die Anzahl an gleichen Zeichen
-            for (long l = 0; l < ac_ASCII.Length; l++)
+            fs_Read = new FileStream(s_file, FileMode.Open);
+            br = new BinaryReader(fs_Read);
+            byte b_sameSigns = 1;                                                               //Zählt die Anzahl an gleichen Zeichen, startet bei 1, da der Ausgangspunkt nicht mitgezählt wird
+            for (long l = 0; l < fs_Read.Length; l++)                                           //Durch die Datei lesen
             {
-                if (l != ac_ASCII.Length - 1)
+                fs_Read.Position = l;                           
+                if (l != fs_Read.Length - 1)                                                    //Wenn es nicht das letzte Zeichen ist
                 {
-                    if (b_sameSigns >= 4 && ac_ASCII[l] != ac_ASCII[l + 1])
+                    if (b_sameSigns >= 4 && br.ReadByte() != br.ReadByte())                     //Wenn das nächste Byte nicht mehr genauso wie das aktuelle ist und sich unser packing lohnt
                     {
+                        fs_Read.Position = l;                                                   //Muss durch die Bedingung der if zurückgestzt werden
                         bw.Write(c_sign);
                         bw.Write(b_sameSigns);
-                        bw.Write(ac_ASCII[l]);
-                        b_sameSigns = 1;
-                        continue;                                //Damit es nicht nochmal des Zeichen von der else darunter schreibt
+                        bw.Write(br.ReadByte());
+                        b_sameSigns = 1;                         
+                        continue;                                                               //Damit es nicht nochmal des Zeichen von der else darunter schreibt
                     }
-                    if (ac_ASCII[l] == ac_ASCII[l + 1])
+                    fs_Read.Position = l;                                                       //Muss durch die Bedingung der if zurückgestzt werden
+                    if (br.ReadByte() == br.ReadByte())                                         //Wenn das darauf folgende Byte das selbe ist
                     {
                         b_sameSigns++;
                     }
-                    else if (b_sameSigns > 1)
+                    else if (b_sameSigns > 1)                                                   //Falls es mehrere Zeichen einer Art sind, aber nicht genug zum packen
                     {
+                        char c = (char)br.ReadByte();
                         for (byte i2 = b_sameSigns; i2 > 0; i2--)
                         {
-                            bw.Write(ac_ASCII[l - i2]);
+                            bw.Write(c);
                         }
                         b_sameSigns = 1;
                     }
-                    else
+                    else                                                                        //Falls es nur 1 Zeichen ist
                     {
-                        bw.Write(ac_ASCII[l]);
+                        fs_Read.Position = l;                                                   //Muss durch die Bedingung der if davor zurückgestzt werden
+                        bw.Write(br.ReadByte());
                         b_sameSigns = 1;
                     }
                 }
-                else if (b_sameSigns >= 4)
+                else if (b_sameSigns >= 4)                                                      //Falls es die letzte Stelle ist und schon Zeichen davor waren
                 {
                     bw.Write(c_sign);
                     bw.Write(b_sameSigns);
-                    bw.Write(ac_ASCII[l]);
+                    char c = (char)br.ReadByte();
+                    for (byte i2 = b_sameSigns; i2 > 0; i2--)
+                    {
+                        bw.Write(c);
+                    }
                 }
                 else
                 {
-                    bw.Write(ac_ASCII[l]);
-                }
+                    bw.Write(br.ReadByte());                                                    //Falls es die letzte Stelle ist und es ein einzelnes Zeichen ist
+                }   
             }
         }
     }
